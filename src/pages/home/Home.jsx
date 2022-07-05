2ad5d269ffaa8449a9121b33f86834252e976013
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-07-05 23:31:05 +0530

    added several components upto newsletter.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,24 @@ import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 
 import './home.css';
+import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
+import MailList from '../../components/mailList/MailList';
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse By Property Type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Home Guests Love</h1>
+        <FeaturedProperties />
+        <MailList />
+      </div>
     </div>
   )
 }
